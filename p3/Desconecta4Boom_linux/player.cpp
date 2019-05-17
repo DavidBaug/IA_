2d5f491c81188730d332dcbb090e2543e4a49d73
David Gil Bautista <davidgilbautista@gmail.com>
2019-05-17 17:06:15 +0200

    Update player.cpp

--- p3/Desconecta4Boom_linux/player.cpp
+++ p3/Desconecta4Boom_linux/player.cpp
@@ -57,6 +57,7 @@ double ValoracionTest(const Environment &estado, int jugador){
 
 // Funcion heuristica (ESTA ES LA QUE TENEIS QUE MODIFICAR)
 double Valoracion(const Environment &estado, int jugador){
+
 }
 
 
@@ -86,7 +87,7 @@ Environment::ActionType Player::Think(){
     const int PROFUNDIDAD_MINIMAX = 6;  // Umbral maximo de profundidad para el metodo MiniMax
     const int PROFUNDIDAD_ALFABETA = 8; // Umbral maximo de profundidad para la poda Alfa_Beta
 
-    Environment::ActionType accion; // acción que se va a devolver
+    Environment::ActionType accion; // acciï¿½n que se va a devolver
     bool aplicables[8]; // Vector bool usado para obtener las acciones que son aplicables en el estado actual. La interpretacion es
                         // aplicables[0]==true si PUT1 es aplicable
                         // aplicables[1]==true si PUT2 es aplicable
@@ -155,3 +156,5 @@ Environment::ActionType Player::Think(){
     return accion;
 }
 
+
+double Poda_AlfaBeta(Environment actual, int jugador, int algo, int profundidad, Environment::ActionType accion, int alfa, int beta){}
